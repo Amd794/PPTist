ab750cce8bdc7dc4afb478b10bda267d49c6459c
AMD794 <2952277346@qq.com>
2025-05-19 15:34:29 +0800

    chore: more tools

--- src/views/Editor/EditorHeader/index.vue
+++ src/views/Editor/EditorHeader/index.vue
@@ -19,9 +19,8 @@
           <PopoverMenuItem @click="setDialogForExport('pptx')">导出文件</PopoverMenuItem>
           <PopoverMenuItem @click="resetSlides(); mainMenuVisible = false">重置幻灯片</PopoverMenuItem>
           <PopoverMenuItem @click="openMarkupPanel(); mainMenuVisible = false">幻灯片类型标注</PopoverMenuItem>
-          <PopoverMenuItem @click="goLink('https://github.com/pipipi-pikachu/PPTist/issues')">意见反馈</PopoverMenuItem>
-          <PopoverMenuItem @click="goLink('https://github.com/pipipi-pikachu/PPTist/blob/master/doc/Q&A.md')">常见问题</PopoverMenuItem>
           <PopoverMenuItem @click="mainMenuVisible = false; hotkeyDrawerVisible = true">快捷操作</PopoverMenuItem>
+          <PopoverMenuItem @click="goLink('https://tools.cmdragon.cn/')">更多实用工具</PopoverMenuItem>
         </template>
         <div class="menu-item"><IconHamburgerButton class="icon" /></div>
       </Popover>
@@ -62,7 +61,10 @@
       <div class="menu-item" v-tooltip="'导出'" @click="setDialogForExport('pptx')">
         <IconDownload class="icon" />
       </div>
-      <a class="github-link" v-tooltip="'Copyright © 2020-PRESENT pipipi-pikachu'" href="https://github.com/pipipi-pikachu/PPTist" target="_blank">
+      <div class="menu-item" v-tooltip="'更多实用工具'" @click="goLink('https://tools.cmdragon.cn/')">
+        <div class="menu-item"><IconMore class="icon" /></div>
+      </div>
+      <a class="github-link" href="https://github.com/Amd794/PPTist" target="_blank">
         <div class="menu-item"><IconGithub class="icon" /></div>
       </a>
     </div>
